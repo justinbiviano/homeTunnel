59f68ffb1140229ff690b80401ee0613f1607e02
Justin Biviano <just.biviano@gmail.com>
2026-03-28 20:48:39 +1100

    Working two way with go routines

---
+++ server.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"sync"
+)
+
+func listen(conn net.Conn) {
+	buf := make([]byte, 1024)
+	n, _ := conn.Read(buf)
+	fmt.Println("Client:", string(buf[:n]))
+}
+
+func message(conn net.Conn) {
+	var message string
+	fmt.Scanln(&message)
+	fmt.Print("\033[1A\033[2K")
+	fmt.Println("Server:", message)
+	fmt.Fprintf(conn, message)
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	ln, _ := net.Listen("tcp", ":8080")
+	conn, _ := ln.Accept()
+	defer conn.Close()
+
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		for {
+			listen(conn)
+		}
+	}()
+	go func() {
+		defer wg.Done()
+		for {
+			message(conn)
+		}
+	}()
+
+	wg.Wait()
+}
