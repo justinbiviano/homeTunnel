b4303a7b1ab1d1f96dc77149dfe13131816b78f8
Justin Biviano <just.biviano@gmail.com>
2026-03-30 07:35:42 +1100

    Added Comments, Added Import to Main to use crypto

--- crypto/keys.go
+++ crypto/keys.go
@@ -12,11 +12,14 @@ import (
 
 func GeneratePrivatePublicKeys() ([32]byte, [32]byte, error) {
 	var privateKey, publicKey [32]byte
+
+	// Random Generation of Private Key
 	_, err := rand.Read(privateKey[:])
 	if err != nil {
 		return publicKey, privateKey, fmt.Errorf("Failed to Generate Private Key: %w", err)
 	}
 
+	// Derives Public Keys From Private Keys
 	publicKeyStr, err := curve25519.X25519(privateKey[:], curve25519.Basepoint)
 	if err != nil {
 		return publicKey, privateKey, fmt.Errorf("Failed to Derive the Public Key: %w", err)
@@ -27,11 +30,13 @@ func GeneratePrivatePublicKeys() ([32]byte, [32]byte, error) {
 }
 
 func hashKeys(secret []byte) ([32]byte, [32]byte, error) {
+	// Hashes Keys Using BLAKE2S.
 	reader := hkdf.New(func() hash.Hash {
 		h, _ := blake2s.New256(nil)
 		return h
-	}, secret, nil, []byte("homeTunnel"))
+	}, secret, nil, []byte("homeTunnel")) // Uses the "homeTunnel as a 'mixing key.
 
+	// Seperates Keys into Server and Client Keys
 	var clientKey, serverKey [32]byte
 	reader.Read(clientKey[:])
 	reader.Read(serverKey[:])
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/justinbiviano/homeTunnel/crypto"
+)
+
+func main() {
+	fmt.Println("Nothing Started Here")
+	public, private, _ := crypto.GeneratePrivatePublicKeys()
+
+}
