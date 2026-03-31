2782a2b52a88db174c05199c0204039ee77eccfe
Justin Biviano <just.biviano@gmail.com>
2026-04-01 07:32:57 +1100

    Successful reading of packets

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.26.1
 require golang.org/x/crypto v0.49.0
 
 require (
+	github.com/vishvananda/netlink v1.3.1 // indirect
+	github.com/vishvananda/netns v0.0.5 // indirect
 	golang.org/x/net v0.51.0 // indirect
 	golang.org/x/sys v0.42.0 // indirect
 	golang.zx2c4.com/wintun v0.0.0-20230126152724-0fa3db229ce2 // indirect
--- tun/tun.go
+++ tun/tun.go
@@ -3,6 +3,7 @@ package tunInterface
 import (
 	"fmt"
 
+	"github.com/vishvananda/netlink"
 	"golang.zx2c4.com/wireguard/tun"
 )
 
@@ -15,9 +16,49 @@ func CreateTUN() (tun.Device, error) {
 }
 
 func ReadPacket(device tun.Device) ([]byte, error) {
+	buf := make([]byte, 1500)
+	bufs := [][]byte{buf}
+	sizes := []int{0}
 
+	_, err := device.Read(bufs, sizes, 0)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to read packet %w", err)
+	}
+
+	return bufs[0][:sizes[0]], nil
 }
 
-func WritePacket(device tun.Device, packet, []byte) error {
+func WritePacket(device tun.Device, packet []byte) error {
+	bufs := [][]byte{packet}
 
+	_, err := device.Write(bufs, 0)
+	if err != nil {
+		return fmt.Errorf("Failed writing packet %w:", err)
+	}
+
+	return nil
+}
+
+func ConfigureTUN(name string, ip string) error {
+	link, err := netlink.LinkByName(name)
+	if err != nil {
+		return fmt.Errorf("Failed to find interface %s: %w", name, err)
+	}
+
+	addr, err := netlink.ParseAddr(ip)
+	if err != nil {
+		return fmt.Errorf("Failed to pharse address: %w", err)
+	}
+
+	err = netlink.AddrAdd(link, addr)
+	if err != nil {
+		return fmt.Errorf("Failed to add address: %w", err)
+	}
+
+	err = netlink.LinkSetUp(link)
+	if err != nil {
+		return fmt.Errorf("Failed to being interface up: %w", err)
+	}
+
+	return nil
 }
