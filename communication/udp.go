9c1b0b2df8ca7378e0b61d7ad43bd09ee3cc32bd
Justin Biviano <just.biviano@gmail.com>
2026-04-02 15:15:31 +1100

    UDP communication functions

--- communication/udp.go
+++ communication/udp.go
@@ -9,7 +9,7 @@ import (
 func UdpDial(address string) (net.Conn, error) {
 	conn, err := net.Dial("udp", address)
 	if err != nil {
-		return conn, fmt.Errorf("Failed to connect to %s over udp: %w", address, err)
+		return nil, fmt.Errorf("Failed to connect to %s over udp: %w", address, err)
 	}
 	return conn, nil
 }
@@ -18,17 +18,23 @@ func UdpDial(address string) (net.Conn, error) {
 func UdpListen(port string) (net.PacketConn, error) {
 	conn, err := net.ListenPacket("udp", port)
 	if err != nil {
-		return conn, fmt.Errorf("Failed to connect on port %s over udp: %w", port, err)
+		return nil, fmt.Errorf("Failed to connect on port %s over udp: %w", port, err)
 	}
 	return conn, nil
 }
 
-// // Reads Packets (Serverside)
-// func UdpRead(conn net.PacketConn, buf []byte) (int, net.Addr, error) {
+// Reads Packets (Serverside)
+func UdpRead(conn net.PacketConn) ([]byte, net.Addr, error) {
+	buf := make([]byte, 1420)
+	n, addr, err := conn.ReadFrom(buf)
+	if err != nil {
+		return nil, nil, fmt.Errorf("Failed to read packet: %w", err)
+	}
+	return buf[:n], addr, nil
+}
 
-// }
-
-// // Sends packets (serverside)
-// func UdpSend(conn net.PacketConn, buf []byte, addr net.Addr) error {
-
-// }
+// Sends packets (serverside)
+func UdpSend(conn net.PacketConn, data []byte, addr net.Addr) error {
+	_, err := conn.WriteTo(data, addr)
+	return err
+}
